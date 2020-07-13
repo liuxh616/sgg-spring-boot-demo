678b7163ccbca5cc61b4407d0cfefba36b44522b
liuxh616 <liuxh616@126.com>
2020-07-13 17:00:21 +0800

    提交profiles文件配置demo

---
+++ day01-demo04-config02/src/main/java/com/fengfan/day01demo04config02/Day01Demo04Config02Application.java
@@ -0,0 +1,13 @@
+package com.fengfan.day01demo04config02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day01Demo04Config02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day01Demo04Config02Application.class, args);
+    }
+
+}
---
+++ day01-demo04-config02/src/test/java/com/fengfan/day01demo04config02/Day01Demo04Config02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengfan.day01demo04config02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day01Demo04Config02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
